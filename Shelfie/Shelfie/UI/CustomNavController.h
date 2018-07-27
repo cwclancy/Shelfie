89afbac77887960d832e5d0369a27fb9f289ea3c
Claudia Haddad <claudiaa6499@gmail.com>
2018-07-27 15:38:36 -0700

    UI of navigation bar

---
+++ Shelfie/Shelfie/UI/CustomNavController.h
@@ -0,0 +1,13 @@
+//
+//  CustomNavController.h
+//  Shelfie
+//
+//  Created by Claudia Haddad on 7/27/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomNavController : UINavigationController
+
+@end
