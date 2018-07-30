e5ba45ca7df490af9ed20c4c4633e28d84c53273
Connor Clancy <connorwilliamclancy@gmail.com>
2018-07-30 12:32:23 -0700

    barcode inheritance in add book view controller

---
+++ Shelfie/Shelfie/UI/BTBarcodeViewController.h
@@ -0,0 +1,15 @@
+//
+//  BarcodeViewController.h
+//  Shelfie
+//
+//  Created by Connor Clancy on 7/30/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BTRevealHeadViewController.h"
+
+
+@interface BTBarcodeViewController : BTRevealHeadViewController
+
+@end
---
+++ Shelfie/Shelfie/UI/BTBarcodeViewController.m
@@ -0,0 +1,73 @@
+//
+//  BarcodeViewController.m
+//  Shelfie
+//
+//  Created by Connor Clancy on 7/30/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import "BTBarcodeViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface BTBarcodeViewController () <AVCaptureMetadataOutputObjectsDelegate>
+@property (strong, nonatomic) UIView *previewView;
+@property (strong, nonatomic) AVCaptureSession *captureSession;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+@end
+
+@implementation BTBarcodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSError *error;
+    self.captureSession = nil;
+    self.previewView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.view addSubview:self.previewView];
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    self.captureSession = [AVCaptureSession new];
+    [self.captureSession addInput:input];
+    AVCaptureMetadataOutput *captureMetaDataOutput = [AVCaptureMetadataOutput new];
+    [self.captureSession addOutput:captureMetaDataOutput];
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    [captureMetaDataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    [captureMetaDataOutput setMetadataObjectTypes:[NSArray arrayWithObject:AVMetadataObjectTypeEAN13Code]];
+    self.videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    [self.videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [self.videoPreviewLayer setFrame:self.previewView.layer.bounds];
+    [self.previewView.layer addSublayer:self.videoPreviewLayer];
+    [self.captureSession startRunning];
+}
+
+-(void)captureOutput:(AVCaptureOutput *)output didOutputMetadataObjects:(NSArray<__kindof AVMetadataObject *> *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
+    if (metadataObjects != nil && [metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        if ([[metadataObj type] isEqualToString:AVMetadataObjectTypeEAN13Code]) {
+            [self performSelectorOnMainThread:@selector(stopReading) withObject:[metadataObj stringValue] waitUntilDone:NO];
+        }
+    }
+}
+
+-(void)stopReading {
+    [self.captureSession stopRunning];
+    self.captureSession = nil;
+    [self.videoPreviewLayer removeFromSuperlayer];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
