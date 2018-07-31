c489ab09add39467565aa758382f10eac07e1fff
Chaliana Rolon <chaliana.rolon@upr.edu>
2018-07-31 13:21:01 -0700

    User Model implementation begun

---
+++ Shelfie/Shelfie/Models/BTUser.h
@@ -0,0 +1,25 @@
+//
+//  BTUser.h
+//  Shelfie
+//
+//  Created by Chaliana Rolon on 7/30/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import "PFObject.h"
+#import <Parse/Parse.h>
+
+@interface BTUser : PFObject <NSCoding, PFSubclassing>
+
+// USER PROPERTIES
+@property (strong, nonatomic) NSString *userId;
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *picture;
+@property (strong, nonatomic) NSMutableArray *booksHave;
+@property (strong, nonatomic) NSMutableArray *booksWant;
+@property (strong, nonatomic) NSMutableArray *booksSell;
+@property (strong, nonatomic) NSMutableArray *booksTrade;
+
+// USER METHODS
+
+@end
---
+++ Shelfie/Shelfie/Models/BTUser.m
@@ -0,0 +1,48 @@
+//
+//  BTUser.m
+//  Shelfie
+//
+//  Created by Chaliana Rolon on 7/30/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import "BTUser.h"
+
+@implementation BTUser
+
+@synthesize userId;
+@synthesize name;
+@synthesize picture;
+@synthesize booksHave;
+@synthesize booksSell;
+@synthesize booksWant;
+@synthesize booksTrade;
+
++ (nonnull NSString *) parseClassName {
+    return @"User";
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:userId forKey:@"userId"];
+    [aCoder encodeObject:name forKey:@"name"];
+    [aCoder encodeObject:picture forKey:@"picture"];
+    [aCoder encodeObject:booksTrade forKey:@"booksTrade"];
+    [aCoder encodeObject:booksHave forKey:@"booksHave"];
+    [aCoder encodeObject:booksWant forKey:@"booksWant"];
+    [aCoder encodeObject:booksSell forKey:@"booksSell"];
+}
+
+- (instancetype) initWithCoder:(NSCoder *)aDecoder {
+    if (self == [super init]) {
+        self.userId = [aDecoder decodeObjectForKey:@"userId"];
+        self.name = [aDecoder decodeObjectForKey:@"name"];
+        self.picture = [aDecoder decodeObjectForKey:@"picture"];
+        self.booksTrade = [aDecoder decodeObjectForKey:@"booksTrade"];
+        self.booksSell = [aDecoder decodeObjectForKey:@"booksSell"];
+        self.booksWant = [aDecoder decodeObjectForKey:@"booksWant"];
+        self.booksHave = [aDecoder decodeObjectForKey:@"booksHave"];
+    }
+    return self;
+}
+
+@end
