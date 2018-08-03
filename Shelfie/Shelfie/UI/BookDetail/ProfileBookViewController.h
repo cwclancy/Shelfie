916a1032406f4fdcd56d21a97cec50d983ca5b17
Claudia Haddad <claudiaa6499@gmail.com>
2018-08-03 13:45:59 -0700

    removed request vc and fixed UI

---
+++ Shelfie/Shelfie/UI/BookDetail/ProfileBookViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileBookViewController.h
+//  Shelfie
+//
+//  Created by Claudia Haddad on 8/3/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileBookViewController : UIViewController
+
+@end
---
+++ Shelfie/Shelfie/UI/BookDetail/ProfileBookViewController.m
@@ -0,0 +1,37 @@
+//
+//  ProfileBookViewController.m
+//  Shelfie
+//
+//  Created by Claudia Haddad on 8/3/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import "ProfileBookViewController.h"
+
+@interface ProfileBookViewController ()
+
+@end
+
+@implementation ProfileBookViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
