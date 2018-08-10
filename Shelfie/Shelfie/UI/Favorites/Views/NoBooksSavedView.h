3c6219fcef8351c7440b29f7bd21ae6c83258379
Connor Clancy <connorwilliamclancy@gmail.com>
2018-08-10 10:17:37 -0700

    saved default page

---
+++ Shelfie/Shelfie/UI/Favorites/Views/NoBooksSavedView.h
@@ -0,0 +1,13 @@
+//
+//  NoBooksSavedView.h
+//  Shelfie
+//
+//  Created by Connor Clancy on 8/10/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoBooksSavedView : UIView
+
+@end
---
+++ Shelfie/Shelfie/UI/Favorites/Views/NoBooksSavedView.m
@@ -0,0 +1,21 @@
+//
+//  NoBooksSavedView.m
+//  Shelfie
+//
+//  Created by Connor Clancy on 8/10/18.
+//  Copyright © 2018 BookTrader. All rights reserved.
+//
+
+#import "NoBooksSavedView.h"
+
+@implementation NoBooksSavedView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
